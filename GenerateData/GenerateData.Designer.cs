b7cf10dce2357c97eee3bb3f25dede17da6c1df2
charles-lei <charles.lei@activenetwork.com>
2016-01-03 17:06:55 +0800

    CreateProject

---
+++ GenerateData/GenerateData.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace GenerateData
+{
+    partial class GenerateData
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(391, 127);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(120, 55);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "OpenDataSource";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(34, 145);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(305, 21);
+            this.txtPath.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(391, 230);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(120, 55);
+            this.btnProcess.TabIndex = 2;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // GenerateData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 392);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "GenerateData";
+            this.Text = "GenerateData";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnProcess;
+    }
+}
+
---
+++ GenerateData/GenerateData.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace GenerateData
+{
+    public partial class GenerateData : Form
+    {
+        public GenerateData()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = null;
+            openFileDialog1.Filter = "EXCLE(*.XLSX)|*.XLSX";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.OpenFile() != null)
+            {
+                txtPath.Text = openFileDialog1.FileName;
+
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            XSSFWorkbook wk;
+            using (FileStream fs = File.OpenRead(txtPath.Text))   //打开myxls.xls文件
+            {
+                 wk = new XSSFWorkbook(fs);   //把xls文件中的数据写入wk中
+               
+            }
+            ISheet sheetLead = wk.GetSheet("Lead");
+            if (sheetLead == null)
+            {
+                MessageBox.Show("Cannot find 'Lead' sheet");
+                return;
+            }
+            ISheet sheetClosedOpp = wk.GetSheet("Closed Opp");
+            if (sheetClosedOpp == null)
+            {
+                MessageBox.Show("Cannot find 'Closed Opp' sheet");
+                return;
+            }
+            ISheet sheetPipeline = wk.GetSheet("Pipeline");
+            if (sheetPipeline == null)
+            {
+                MessageBox.Show("Cannot find 'Pipeline' sheet");
+                return;
+            }
+            var leadData = GetSheetData(sheetLead);
+            var closedOppData = GetSheetData(sheetClosedOpp);
+            var pipelineData = GetSheetData(sheetPipeline);
+            var verticals = new List<string>();
+            leadData.ForEach(l => {
+                if (!verticals.Contains(l[0]))
+                {
+                    verticals.Add(l[0]);
+                }
+            });
+            closedOppData.ForEach(l => {
+                if (!verticals.Contains(l[0]))
+                {
+                    verticals.Add(l[0]);
+                }
+            });
+            pipelineData.ForEach(l => {
+                if (!verticals.Contains(l[0]))
+                {
+                    verticals.Add(l[0]);
+                }
+            });
+           
+            verticals.Add("Grand Total");
+            //创建工作薄
+            XSSFWorkbook wkNew = new XSSFWorkbook();
+            List<string> leadStatus = new List<string>{ "Leads", "New","Working","Accepted","Converted","Rejected" };
+            Dictionary<string,string> pipelineStatus = new Dictionary<string, string> { { "ClosedWon", "Closed Won" }, { "ClosedLost", "Closed Lost" }, { "ClosedWonM", "Closed Won" }, { "ClosedLostM", "Closed Lost" }, { "PipelineCreated", "" } };
+            //创建一个名称为mySheet的表
+            ISheet tb = wkNew.CreateSheet("ReferenceData");
+            for (int i = 0; i< verticals.Count;i++)
+            {
+                //创建一行，此行为第二行
+                for (int j = 0; j < leadStatus.Count; j++)
+                {
+                    IRow row = tb.CreateRow(i* (leadStatus.Count+pipelineStatus.Count)+j);
+                    ICell cell = row.CreateCell(0);  //在第二行中创建单元格
+                    cell.SetCellValue(verticals[i]);//循环往第二行的单元格中添加数据
+
+                    ICell cell1 = row.CreateCell(1);
+                    cell1.SetCellValue(leadStatus[j]);
+
+                    for (int k = 2; k < 12 + 2; k++)
+                    {
+                        int data = 0;
+                        ICell cellData = row.CreateCell(k);
+                        if (verticals[i] == "Grand Total")
+                        {
+                            if (j == 0)
+                                data = leadData.Where(L => L[1].StartsWith((k - 1).ToString() + "/")).Count();
+                            else
+                                data = leadData.Where(L => L[1].StartsWith((k - 1).ToString() + "/") && L[2] == leadStatus[j]).Count();
+                            cellData.SetCellValue(data);
+                        }
+                        else {
+                            if (j == 0)
+                                data = leadData.Where(L => L[0] == verticals[i] && L[1].StartsWith((k - 1).ToString() + "/")).Count();
+                            else
+                                data = leadData.Where(L => L[0] == verticals[i] && L[1].StartsWith((k - 1).ToString() + "/") && L[2] == leadStatus[j]).Count();
+                            cellData.SetCellValue(data);
+                        }
+                    }
+                }
+                for (int j = 0; j < pipelineStatus.Count;j++)
+                {
+                    IRow row = tb.CreateRow(i * (leadStatus.Count + pipelineStatus.Count)+leadStatus.Count + j);
+                    ICell cell = row.CreateCell(0);  //在第二行中创建单元格
+                    cell.SetCellValue(verticals[i]);//循环往第二行的单元格中添加数据
+
+                    ICell cell1 = row.CreateCell(1);
+                    cell1.SetCellValue(pipelineStatus.ElementAt(j).Key);
+
+                    for (int k = 2; k < 12 + 2; k++)
+                    {
+                        ICell cellData = row.CreateCell(k);
+                        if (j == 0 || j == 1)
+                        {
+                            if(verticals[i]=="Grand Total")
+                            {
+                                int data = closedOppData.Where(L=> L[1].StartsWith((k - 1).ToString() + "/") && L[2] == pipelineStatus.ElementAt(j).Value).Count();
+                                cellData.SetCellValue(data);
+                            }
+                            else
+                            {
+                                int data = closedOppData.Where(L => L[0] == verticals[i] && L[1].StartsWith((k - 1).ToString() + "/") && L[2] == pipelineStatus.ElementAt(j).Value).Count();
+                                cellData.SetCellValue(data);
+                            }
+                        }
+                        else if (j == 2 || j == 3)
+                        {
+                            if (verticals[i] == "Grand Total")
+                            {
+                                double data = closedOppData.Where(L => L[1].StartsWith((k - 1).ToString() + "/") && L[2] == pipelineStatus.ElementAt(j).Value).Sum(L =>
+                                {
+                                    double ret = 0.0;
+                                    double.TryParse(L[4], out ret);
+                                    return ret;
+                                });
+                                cellData.SetCellValue(data);
+                            }
+                            else
+                            {
+                                double data = closedOppData.Where(L => L[0] == verticals[i] && L[1].StartsWith((k - 1).ToString() + "/") && L[2] == pipelineStatus.ElementAt(j).Value).Sum(L =>
+                                {
+                                    double ret = 0.0;
+                                    double.TryParse(L[4], out ret);
+                                    return ret;
+                                });
+                                cellData.SetCellValue(data);
+                            }
+                        }
+                        else
+                        {
+                            if (verticals[i] == "Grand Total")
+                            {
+                                double data = pipelineData.Where(L => L[1].StartsWith((k - 1).ToString() + "/")).Sum(L => { double ret = 0.0; double.TryParse(L[4], out ret); return ret; });
+                                cellData.SetCellValue(data);
+                            }
+                            else
+                            {
+                                double data = pipelineData.Where(L => L[0] == verticals[i] && L[1].StartsWith((k - 1).ToString() + "/")).Sum(L => { double ret = 0.0; double.TryParse(L[4], out ret); return ret; });
+                                cellData.SetCellValue(data);
+                            }
+                        }
+                    }
+                }
+               
+            }
+
+
+            using (FileStream fs1 = File.OpenWrite(Application.StartupPath+@"/myxls.xlsx")) //打开一个xls文件，如果没有则自行创建，如果存在myxls.xls文件则在创建是不要打开该文件！
+            {
+                wkNew.Write(fs1);   //向打开的这个xls文件中写入mySheet表并保存。
+                MessageBox.Show("提示：创建成功！");
+            }
+        }
+
+        private List<List<string>> GetSheetData(ISheet sheet)
+        {
+            var rows = new List<List<string>>();
+            if (sheet == null)
+                return rows;
+            for (int i = 1; i <= sheet.LastRowNum; i++)
+            {
+                IRow row = sheet.GetRow(i);
+                if (row != null)
+                {
+                    var leadRow = new List<string>();
+                    for (int j = 0; j <= row.LastCellNum; j++)
+                    {
+                        ICell cell = row.GetCell(j);
+                        if (cell != null)
+                        {
+                            leadRow.Add(cell.ToString());
+                        }
+                    }
+                    if(leadRow.Count>=4 && !leadRow.All(s=>string.IsNullOrEmpty(s)))
+                        rows.Add(leadRow);
+                }
+            }
+            return rows;
+        }
+    }
+}
